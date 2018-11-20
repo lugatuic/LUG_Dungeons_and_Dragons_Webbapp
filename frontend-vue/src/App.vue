c65bd13619184255bbd64299d8deb55173b4307e
Joshua Castor <jcasto3@uic.edu>
2018-11-20 15:05:07 -0600

    refactored app bar into its own component

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <app-bar/>
 
     <v-content>
       <router-view/>
@@ -23,9 +9,13 @@
 </template>
 
 <script>
+import AppBar from '@/components/AppBar';
 
 export default {
   name: 'App',
+  components: {
+    AppBar,
+  },
   data () {
     return {
       //
---
+++ frontend-vue/src/components/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>LUG</span>
+      <span class="font-weight-light" v-text="'D&D Web App'"/>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn flat>
+      <span class="mr-2">Login</span>
+      <v-icon>person</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
