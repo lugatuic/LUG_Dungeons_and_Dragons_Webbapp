1cc034c609b16cd86d5ea78c73505a14fb97763d
Joshua Castor <jcasto3@uic.edu>
2018-11-30 15:54:18 -0600

    default to light theme

--- frontend-vue/src/store/index.js
+++ frontend-vue/src/store/index.js
@@ -9,7 +9,7 @@ export default new Vuex.Store({
     characters,
   },
   state: {
-    useLightTheme: false,
+    useLightTheme: true,
   },
   mutations: {
     setLightThemeUsage (state, useLightTheme) {
