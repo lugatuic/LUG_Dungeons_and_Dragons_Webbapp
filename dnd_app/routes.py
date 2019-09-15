da8f9239b3d4dc17dab56314add4c96e2a54d194
Benbigmac <benmaciorowski@yahoo.com>
2019-09-15 15:06:39 -0500

    Reseting the code for the new group to start clean

---
+++ dnd_app/routes.py
@@ -0,0 +1,54 @@
+from flask import Flask, flash, redirect, render_template, request, url_for
+from app import app
+from app.forms import LoginForm, RegistrationForm
+
+
+@app.route('/')
+def home:
+    return render_template('login.html', title='Home')
+
+
+@app.route('/login' methods=['POST', 'GET'])
+def login():
+    form = LoginForm()  # username +password
+    if request.method == 'POST':  # TODO
+        print 'POST'
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/createAccount' methods=['POST', 'GET'])
+def accountCreate():  # needs Username password, email
+    # TODO
+    form = RegistrationForm()
+    return render_template('accountCreation.html', title='Create Account', form=form)
+
+
+@app.route('/<user>/createCharacter' methods=['POST', 'GET'])
+def characterCreate(user):
+    # TODO
+    return render_template('characterCreation.html', title='Create Character')
+
+
+@app.route('/<user>/createItem' methods=['POST', 'GET'])
+def itemCreate(user):  # Name, cost, wieght, Availabity +flavor text/ability/uses
+    # TODO
+    return render_template('itemCreation.html', title='Create item')
+
+
+@app.route('/<user>/createWeapon' methods=['POST', 'GET'])
+def itemCreate(user):  # Name, Class, Range, Damage, Penetration, Special, Weight, Cost, Availabity, Flavor
+    # TODO
+    return render_template('weaponCreation.html', title='Create Weapon')
+
+
+@app.route('/<user>/createGame' methods=['POST', 'GET'])
+def gameCreate(user):
+    if request.method == 'POST':
+        gameFile = {"game_name":"","Dungeon_Master":"","gameID":4,"Users":[],"msgLog":[]}
+    # TODO
+    return render_template('gameCreation.html', title='Create game')
+
+@app.route('/<user>/modifyGame' methods=['POST', 'GET'])
+def gameModify(user,game):
+    # TODO
+    return render_template('modifyGame.html', title='Create game')
