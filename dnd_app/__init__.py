39bdf8af9cf750e849d35349736b9f4fcadf84e5
root <chasty2@uic.edu>
2020-01-27 20:29:46 -0600

    -used flask-mysqldb to connect flask app to database in docker container mysql:latest 	my reasoning was that it is a small wrapper for mysqlclient(from what ive read, 	its the fastest) that handles best practice connections for flask while also 	allowing us to do mysql queries in the code. -changed registration form in routes.py to register users 	Needs work, but it functions. Note that it does not work from localhost:5000/ 	but it does from localhost:5000/createAccount

--- dnd_app/__init__.py
+++ dnd_app/__init__.py
@@ -1,8 +1,17 @@
 """Initialize Flask Server"""
 from flask import Flask
+from flask_mysqldb import MySQL
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+# config mysql database
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'test'
+app.config['MYSQL_DB'] = 'lugdb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 from dnd_app import routes
