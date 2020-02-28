fbcd3d7ffe9d5c291349a1d4d0668947d170a4e0
chasty2 <chasty2@uic.edu>
2020-02-28 12:29:11 -0600

    building db take 2

--- initDB/build_db.sql
+++ initDB/build_db.sql
@@ -1,6 +1,3 @@
-ALTER USER 'root'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY "test";
-CREATE DATABASE lugdb;
-
 use lugdb;
 
 CREATE TABLE account(
@@ -9,4 +6,30 @@ CREATE TABLE account(
     password VARCHAR(100) NOT NULL,
     email VARCHAR(254) NOT NULL,
     PRIMARY KEY ( id )
+);
+
+CREATE TABLE charSheet(
+    charid INT NOT NULL AUTO_INCREMENT
+    name VARCHAR(100) NOT NULL,
+    race VARCHAR(20) NOT NULL,
+    lore VARCHAR(1000),
+    ideals VARCHAR(254),
+    bonds VARCHAR(254),
+    flaws VARCHAR(254),
+    id int,
+    PRIMARY KEY (charid)
+    FOREIGN KEY (id) REFERENCES account(id)
+);
+
+CREATE TABLE classlevel(
+    class VARCHAR(50) NOT NULL,
+    hitDie VARCHAR(10) NOT NULL,
+    classlvl INT NOT NULL,
+    charid INT,
+    PRIMARY KEY(class)
+    FOREIGN KEY (charid) REFERENCES charSheet(charid)
+);
+
+CREATE TABLE basestats(
+
 );
