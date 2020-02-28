9aa7f407daf9fe26209050f7335af1b4eb406b1c
chasty2 <chasty2@uic.edu>
2020-02-28 15:00:40 -0600

    working db version 2

--- initDB/build_db.sql
+++ initDB/build_db.sql
@@ -9,16 +9,19 @@ CREATE TABLE account(
 );
 
 CREATE TABLE charSheet(
-    charid INT NOT NULL AUTO_INCREMENT
+    charid INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     race VARCHAR(20) NOT NULL,
     lore VARCHAR(1000),
     ideals VARCHAR(254),
     bonds VARCHAR(254),
     flaws VARCHAR(254),
-    id int,
-    PRIMARY KEY (charid)
-    FOREIGN KEY (id) REFERENCES account(id)
+    id INT,
+    PRIMARY KEY (charid),
+    FOREIGN KEY (id)
+        REFERENCES account(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 CREATE TABLE classlevel(
@@ -26,10 +29,26 @@ CREATE TABLE classlevel(
     hitDie VARCHAR(10) NOT NULL,
     classlvl INT NOT NULL,
     charid INT,
-    PRIMARY KEY(class)
-    FOREIGN KEY (charid) REFERENCES charSheet(charid)
+    PRIMARY KEY (class),
+    FOREIGN KEY (charid)
+        REFERENCES charSheet(charid)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 CREATE TABLE basestats(
-
+    strength INT NOT NULL,
+    dexterity INT NOT NULL,
+    constitution INT NOT NULL,
+    intelligence INT NOT NULL,
+    wisdom INT NOT NULL,
+    charisma INT NOT NULL,
+    proficiency INT NOT NULL,
+    maxhp INT NOT NULL,
+    movespeed INT NOT NULL,
+    charid INT,
+    FOREIGN KEY (charid)
+        REFERENCES charSheet(charid)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
