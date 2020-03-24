132280787fedde7ad0e0d288db8bc35ab7b6c47a
chasty2 <chasty2@uic.edu>
2020-03-23 19:28:04 -0500

    initialized flask-login

--- dnd_app/__init__.py
+++ dnd_app/__init__.py
@@ -1,11 +1,13 @@
 """Initialize Flask Server"""
 from flask import Flask
 from flask_mysqldb import MySQL
+from flask_login import LoginManager
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# config mysql database
+## Config mysql database.
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_USER'] = 'root'
@@ -13,5 +15,7 @@ app.config['MYSQL_PASSWORD'] = 'test'
 app.config['MYSQL_DB'] = 'lugdb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
+## Config flask-login
+login = LoginManager(app)
 
 from dnd_app import routes
