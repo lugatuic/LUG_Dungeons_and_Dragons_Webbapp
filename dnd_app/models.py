22f99536b1b69082bcbf0d3ece65fd02653fd531
chasty2 <chasty2@uic.edu>
2020-03-23 21:53:04 -0500

    added User class and user loader

--- dnd_app/models.py
+++ dnd_app/models.py
@@ -6,29 +6,15 @@ from dnd_app import login, mysql
 
 class User(UserMixin):
     '''Database model to hold user information from the database'''
-    id = 0
-    account_name = ''
-    password = ''
-    email = ''
-
-    def load_with_id(self, account_id):
+    def __init__(self, key):
         '''Loads User object from account id'''
         ## Run mysql query for account relation.
         cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM account WHERE id=%s", (account_id,))
-        uservalues = cursor.fetchone()
-        cursor.close()
-        ## Populate Object.
-        self.id = uservalues['id']
-        self.account_name = uservalues['accountName']
-        self.password = uservalues['password']
-        self.email = uservalues['email']
-
-    def load_with_login(self, account_name):
-        '''Loads User object from username'''
-        ## Run mysql query for account relation.
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM account WHERE accountName=%s", (account_name,))
+        ## search for id if key is int, search for accountName if string.
+        if isinstance(key, int) is True:
+            cursor.execute("SELECT * FROM account WHERE id=%s", (key,))
+        else:
+            cursor.execute("SELECT * FROM account WHERE accountName=%s", (key,))
         uservalues = cursor.fetchone()
         cursor.close()
         ## Populate Object.
@@ -40,4 +26,4 @@ class User(UserMixin):
 
 @login.user_loader
 def load_user(id):
-    return User().load_with_id(int(id))
+    return User(int(id))
